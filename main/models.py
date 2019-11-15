8d5935fb1c35892e49cb56f794e38c007ec55d91
azure0309 <lifeofazure@gmail.com>
2019-11-15 18:21:16 +0800

    last 11-15

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large')
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
