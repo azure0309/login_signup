3afd7a56b85239911d093622facf58d5facd9371
azure0309 <lifeofazure@gmail.com>
2019-11-21 18:25:32 +0800

    Azure

--- main/views.py
+++ main/views.py
@@ -6,6 +6,6 @@ from .models import ToDoList, Item
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, 'main/base.html', {'ls': ls})
+    return render(response, 'main/list.html', {'ls': ls})
 def home(response):
     return render(response, 'main/home.html', {"name": "test"})
